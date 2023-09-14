2b97dea09dd7a32dba42be2c80370eaaa15563d7
jtcarlos <john.thomas.carlos0913@gmail.com>
2023-09-14 13:45:17 +0800

    used default stack navigator

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import { screens } from "@core/screens";
 import { settings } from "@core/settings";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const App = () => {
   const { navigatorOptions } = settings;
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator {...navigatorOptions}>
-        {screens?.map((screen, index) => (
+        {screens?.map((screen) => (
           <Stack.Screen key={screen?.name} {...screen} />
         ))}
       </Stack.Navigator>
